3c3e39eb752898c4ddbd9b404478d002de44a216
KarolciaP <73311632+Karolcia-P@users.noreply.github.com>
2024-01-07 23:38:30 +0100

    global_notification correction

--- djangoCalendar/MyCalendar/context_processors.py
+++ djangoCalendar/MyCalendar/context_processors.py
@@ -5,14 +5,15 @@ from datetime import timedelta
 def global_notifications(request):
     notifications = []
 
-    projects = Project.objects.filter(user=request.user, completed=False)
-    current_date = timezone.now().date()
-    for project in projects:
-        if project.end_date and current_date <= project.end_date <= current_date + timedelta(days=1):
-            notification = {
-                'title': 'Project is ending soon.',
-                'content': f'Title: {project.title} <br> End Date: {project.end_date.strftime("%B %d, %Y")}',
-            }
-            notifications.append(notification)
+    if request.user.is_authenticated:
+        projects = Project.objects.filter(user=request.user, completed=False)
+        current_date = timezone.now().date()
+        for project in projects:
+            if project.end_date and current_date <= project.end_date <= current_date + timedelta(days=1):
+                notification = {
+                    'title': 'Project is ending soon.',
+                    'content': f'Title: {project.title} <br> End Date: {project.end_date.strftime("%B %d, %Y")}',
+                }
+                notifications.append(notification)
 
     return {'global_notifications': notifications}
